52cab59d08b26f022faafe5441936c361b4f0120
Kirera Wainaina <richardwainainak@gmail.com>
2024-06-08 21:51:09 +0100

    Connect the controller to the model

--- src/main/java/com/learn_spring/app/HomeController.java
+++ src/main/java/com/learn_spring/app/HomeController.java
@@ -1,13 +1,26 @@
 package com.learn_spring.app;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+  record Video(String name) { }
+
+  List<Video> videos = List.of(
+    new Video("Need help learning Spring Boot"),
+    new Video("Learn Spring Boot in 3 days"),
+    new Video("Forget everything you know")
+  );
+
   @GetMapping("/")
-  public String index()
+  public String index(Model model)
   {
+    model.addAttribute("videos", videos);
     return "index";
   }
 }
